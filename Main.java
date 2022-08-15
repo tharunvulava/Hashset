f77e7343627bb2701f661493fad9944145d85901
tharunvulava <90199079+tharunvulava@users.noreply.github.com>
2022-08-15 19:02:59 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    HashSet <Integer> hs=new HashSet();
+        hs.add(30);                 
+        hs.add(9);
+        hs.add(2000);
+        hs.add(8);
+        hs.add(10);
+        hs.add(2006);
+		for(Integer i:hs)
+		{
+		    System.out.println(i);
+		}
+	}
+}
